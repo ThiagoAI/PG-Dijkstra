487d72125d4bb569eca23f743ca5f7c3b946a1d7
ThiagoAI <thiagoandersimhoff@gmail.com>
2017-04-07 15:16:45 -0300

    Heap de Fibonacci sendo implementada, insert e new prontos.

--- fibheap.c
+++ fibheap.c
@@ -1,11 +1,54 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <float.h>
 
 #include "fibheap.h"
 
 //Inicializamos a heap
-/*fib_heap* create_heap(fib_heap* heap){
+fib_heap* create_heap(){
+	fib_heap* new = (fib_heap*)malloc(sizeof(fib_heap));
   heap->n = 0;
   heap->min = NULL;
-}*/
+  return new;
+}
+
+//Inicialização do nodo
+fib_node* create_node(int id){
+	fib_node* new = (fib_node*)malloc(sizeof(fib_node));
+	new->id = id;
+  new->key = DBL_MAX;
+  new->mark = 0;
+  new->degree = 0;
+	new->parent = NULL;
+  new->child = NULL;
+  new->left = NULL;
+  new->right = NULL;
+  return new;
+}
+
+//Insere nodo na heap
+void insert(fib_heap* heap,fib_node* node){
+
+  //Se a heap está vazia o primeiro novo é o mínimo
+	if(heap->min == NULL){
+		node->right = node;
+		node->left = node;
+		heap->min = node;
+  }
+  else{
+ 		//Inserimos o novo a esquerda do mínimo na root list
+    node->right = min;
+    node->left = min->left;
+    heap->min->left = node;
+    node->left->right = node;
+ 
+		//Se for menor que o mínimo, vira o novo mínimo
+		if(node->key < min->key) heap->min = node;
+	}
+
+	heap->n++;
+}
+
+
+
--- fibheap.h
+++ fibheap.h
@@ -2,30 +2,35 @@
 #define FIBHEAP_H_
 
 //Nodo da heap
-typedef struct fib_node{
-  struct fib_node* left;
-  struct fib_node* right;
-  struct fib_node* child;
-  struct fib_node* parent;
-  int key;
+typedef struct fib_nodestruct{
+  //Ponteiros: left e right são para lista duplamente encadeada circular.
+  //Child se refere a uma das crianças do nodo(só precisa de um), e parent ao pai.
+  struct fib_nodestruct* left;
+  struct fib_nodestruct* right;
+  struct fib_nodestruct* child;
+  struct fib_nodestruct* parent;
+  //Key é o valor do algorítimo de dijkstra até aquele ponto (inicializamos com o maior valor possível)
+  double key;
+  //id é o id do vértice deste nodo
   int id;
+  //degree é para a heap, dita algumas coisas
   int degree;
+  //mark também é para a heap, é usado como boolean
   int mark;
-
-}fibnode;
+}fib_node;
 
 //A heap em si
-typedef struct fib_heap{
+typedef struct fib_heapstruct{
+  //Número de nodos
   int n;
-  fibnode min;
- 
+  //Mínimo da heap
+  fib_node min;
+
 }fib_heap;
 
 //Funções básicas
 fib_heap* create_heap();
-//fib_node* 
-//void add_list(int_list* list,int x);
-//int get_point(int_list* list,int i);
-//int_list* destroy_list(int_list* list);
+fib_node* create_node(int id);
+void insert(fib_heap* heap,fib_node* node);
 
 #endif
